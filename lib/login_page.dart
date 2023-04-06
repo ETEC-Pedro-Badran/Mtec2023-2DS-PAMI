23dbd509710cb9cd821c0e42b7f5380056941d91
evaldo <evaldo.junior@etec.sp.gov.br>
2023-04-06 13:04:01 -0300

    .

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,32 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50.0),
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+                decoration: InputDecoration(
+                    label: Text("Email"),
+                    prefixIcon: Icon(Icons.key),
+                    border: OutlineInputBorder())),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  label: Text("Senha"),
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: Icon(Icons.visibility),
+                  border: OutlineInputBorder()),
+              obscureText: true,
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
